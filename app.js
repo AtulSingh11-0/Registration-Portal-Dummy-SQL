a3c7a0959e6d3921d15ba38b75423a9647e7adee
Atul Singh <cocatul11@gmail.com>
2023-11-21 18:30:22 +0530

    Remove error handling middleware

--- app.js
+++ app.js
@@ -10,11 +10,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/registration', route);
 
-// for error handling, just dont use this as it always shows res.status is not a function
-// app.use((req, res, err, next) => {
-//   console.error(err.stack);
-//   return res.status(500).send('Something Broke!');
-// });
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`App listening at PORT ${PORT}`));
